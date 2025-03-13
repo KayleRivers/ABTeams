5ffa61f457d40070450cdc71006fb2212be8e584
KayleRivers <kayle.rivers@protechture.com>
2025-03-13 14:56:51 -0400

    Update index.js

--- 02.echo-bot/index.js
+++ 02.echo-bot/index.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const path = require('path');
+onst path = require('path');
 
 class SimulatedUser {
     constructor(userId) {
