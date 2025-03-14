c94668ca87b2aa00b3e11a8d6292f6aa2e2d2cd0
KayleRivers <kayle.rivers@protechture.com>
2025-03-14 03:42:16 +0000

    updating the echo bot

--- 02.echo-bot/index.js
+++ 02.echo-bot/index.js
@@ -82,60 +82,30 @@ class EchoBot extends ActivityHandler {
     }
 }
 
-// Create HTTP server
-const server = restify.createServer();
-server.use(restify.plugins.bodyParser());
-
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
-});
-
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
-
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
-const adapter = new CloudAdapter(botFrameworkAuthentication);
-
-// Catch-all for errors.
-const onTurnErrorHandler = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry
-    //       configuration instructions.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
-
-    // Log the context.activity object to debug the issue
-    console.log('Context activity on error:', context.activity);
-
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
-
-    // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
-};
-
-// Set the onTurnError for the singleton CloudAdapter.
-adapter.onTurnError = onTurnErrorHandler;
-
-// Create the main dialog.
 const myBot = new EchoBot();
 
 // Listen for incoming requests
 server.post('/api/messages', async (req, res) => {
-    await adapter.process(req, res, (context) => myBot.run(context));
+    await adapter.process(req, res, async (context) => {
+        try {
+            await myBot.run(context);
+        } catch (error) {
+            console.error('Error processing request:', error);
+            await context.sendActivity('There was an error processing your request.');
+        }
+    });
 });
 
 // Listen for Upgrade requests for Streaming
 server.on('upgrade', async (req, socket, head) => {
     const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
     streamingAdapter.onTurnError = onTurnErrorHandler;
-    await streamingAdapter.process(req, socket, head, (context) => myBot.run(context));
+    await streamingAdapter.process(req, socket, head, async (context) => {
+        try {
+            await myBot.run(context);
+        } catch (error) {
+            console.error('Error processing request:', error);
+            await context.sendActivity('There was an error processing your request.');
+        }
+    });
 });
